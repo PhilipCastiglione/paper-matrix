e4c9e66a34120d668d4fc49bee37134da00390f8
Philip Castiglione <philipcastiglione@gmail.com>
2024-11-26 19:32:00 +0800

    add actiontext, notes is now a rich text field

--- test/models/paper_test.rb
+++ test/models/paper_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: papers
+#
+#  id           :integer          not null, primary key
+#  authors      :string
+#  auto_summary :text
+#  notes        :text
+#  read         :boolean          default(FALSE)
+#  title        :string
+#  url          :string
+#  year         :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 require "test_helper"
 
 class PaperTest < ActiveSupport::TestCase
