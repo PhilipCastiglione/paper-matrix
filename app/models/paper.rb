1a076c08c1dedc1b8d70e0b2b79d8860636f8ea2
Philip Castiglione <philipcastiglione@gmail.com>
2024-11-26 19:57:58 +0800

    validate paper url, update forms

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -15,4 +15,22 @@
 #
 class Paper < ApplicationRecord
   has_rich_text :notes
+
+  validate :creation_requirements, on: :create
+
+  private
+
+  def creation_requirements
+    if url.blank?
+      # TODO: we will add support for creation with a file upload in the future
+      errors.add(:base, "Url is required")
+    end
+
+    begin
+      uri = URI.parse(url)
+      errors.add(:base, "Url is invalid") unless (uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)) && !uri.host.nil?
+    rescue URI::InvalidURIError
+      errors.add(:base, "Url is invalid")
+    end
+  end
 end
