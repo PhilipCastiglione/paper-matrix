6c4b92dbd23e2e4fd7305e777b99da6f02c8ce14
Philip Castiglione <philipcastiglione@gmail.com>
2024-12-10 10:07:07 +0800

    deployment configuration

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,4 +1,4 @@
 OpenAI.configure do |config|
-  config.access_token = Rails.application.credentials.openai[:access_token]
+  config.access_token = Rails.application.credentials.openai.fetch(:access_token, "")
   config.log_errors = true
 end
